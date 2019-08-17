69062e0201fba0fd00e78d06129155e84a666571
ghost717 <dawid.zawadzki7717@gmail.com>
2019-08-18 00:54:59 +0200

    service fix

---
+++ src/app/star-wars.service.ts
@@ -0,0 +1,21 @@
+export class StarWarsService {
+  private characters = [
+    { name: 'Luke Skylwalker', side: ''},
+    { name: 'Darth Vader', side: ''}
+  ];
+
+  getCharacters(chosenList) {
+    if (this.chosenList === 'all') {
+      return this.characters.slice();
+    }
+    return this.characters.filter((char) => {
+      return char.side === chosenList;
+    })
+  }
+  onSideChosen(charInfo) {
+    const pos = this.characters.findIndex((char) => {
+      return char.name === charInfo.name;
+    })
+    this.characters[pos].side = charInfo.side;
+  }
+}
