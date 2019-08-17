8e5b8fe1d4d2e78dbf43f31b179639d9f975b180
ghost717 <dawid.zawadzki7717@gmail.com>
2019-08-17 20:40:50 +0200

    generate component -> ng g c user-detail

--- src/app/user.component.ts
+++ src/app/user.component.ts
@@ -7,6 +7,7 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
     <!-- <input type="text" [(ngModel)]="name"> -->
     <p>Hello {{ name }}!</p>
     <p>I'm UserComponent</p>
+    <app-user-detail></app-user-detail>
   `
 })
 export class UserComponent {
