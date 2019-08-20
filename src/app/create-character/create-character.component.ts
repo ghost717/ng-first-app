46516aa2a90197b76edad31d081b7dbd0c60d722
ghost717 <dawid.zawadzki7717@gmail.com>
2019-08-20 23:59:02 +0200

    ngModel

--- src/app/create-character/create-character.component.ts
+++ src/app/create-character/create-character.component.ts
@@ -13,6 +13,7 @@ export class CreateCharacterComponent implements OnInit {
     {display: 'Dark', value: 'dark'}
   ];
   swService: StarWarsService;
+  defaultName = 'Obi-Wan';
 
   constructor(swService: StarWarsService) {
     this.swService = swService;
